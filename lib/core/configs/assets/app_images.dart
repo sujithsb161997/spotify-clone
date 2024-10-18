c8e2df39fcb5db741141eda0f460cefceb264907
Sujith <sujith@richkenmedia.com>
2024-10-18 17:57:02 +0530

    choose mode ui completed.

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -2,6 +2,7 @@ class AppImages {
     static const String basePath = 'assets/images/';
 
 
-    static const String introBG = '${basePath}intro_bg.png';
 
+    static const String introBG = '${basePath}intro_bg.png';
+    static const String chooseModeBG = '${basePath}choose_mode_bg.png';
 }
--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:spotify_clone/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_clone/core/configs/assets/app_images.dart';
 import 'package:spotify_clone/core/configs/assets/app_vectors.dart';
+import 'package:spotify_clone/presentation/choose_mode/pages/choose_mode.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -20,45 +21,55 @@ class GetStartedPage extends StatelessWidget {
                 image: AssetImage(AppImages.introBG),
               ),
             ),
-            child: Column(
-              children: [
-                Align(
-                    alignment: Alignment.topCenter,
-                    child: SvgPicture.asset(AppVectors.logo)),
-                const Spacer(),
-                const Text(
-                  "Enjoy Listening To Music",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 18),
-                ),
-                const SizedBox(
-                  height: 21,
-                ),
-                const Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
-                    fontSize: 13,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                BasicAppButton(
-                  onPressed: () {},
-                  title: "Get Started",
-                  
-                )
-              ],
-            ),
           ),
           Container(
             color: Colors.black.withOpacity(0.15),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+            child: Column(
+                children: [
+                  Align(
+                      alignment: Alignment.topCenter,
+                      child: SvgPicture.asset(AppVectors.logo)),
+                  const Spacer(),
+                  const Text(
+                    "Enjoy Listening To Music",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 18),
+                  ),
+                  const SizedBox(
+                    height: 21,
+                  ),
+                  const Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                      fontSize: 13,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  BasicAppButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => const ChooseModePage(),
+                        ),
+                      );
+                    },
+                    title: "Get Started",
+                  )
+                ],
+              ),
+          ),
+         
         ],
       ),
     );
